5833101462b8a5feb596a8045646879c256b9981
guanlong zhang <2759819042@qq.com>
2023-11-21 18:18:26 +0800

    add

--- C++11_NewFeature/test2.cpp
+++ C++11_NewFeature/test2.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<list>
+#include<map>
 using namespace std;
 
 class T3 {
@@ -106,16 +107,115 @@ public:
 };
 
 //模板的优化
+//模板的右尖括号
+template<class T>
+class Container {
+public:
+	void printContainer(T&t) {
+		for (auto it = t.begin(); it != t.end(); it++) {
+			cout << "key:" << (*it).first << "  value:" << (*it).second << endl;
+		}
+	}
+};
+
+//函数模板添加默认参数   C++11之前不可以进行添加，C++11可以
+template<typename T0 = long, typename T1 = int>
+void myTest(T0 t0 = 'A', T1 t1 = 'B'){
+	cout << "t0:" << t0 << "  t1:" << t1 << endl;
+}
+
 void test6() {
+	map<int,string> ma;
+	ma.insert(make_pair(1, "zxc"));
+	ma.insert(make_pair(2, "asd"));
+	ma.insert(make_pair(3, "qwe"));
+	Container<map<int, string>> Con;    //在C++11之前模板的>>会将其识别为右移运算符，C++11就不会出现这种情况了
+	Con.printContainer(ma);
+
+	myTest('a', 'b');              //自动类型推导   char char
+	myTest<int>('a', 'b');         //int char
+	myTest<char>('a', 'b');        //char char
+	myTest<int, char>('a', 'b');   //显示指定
+	myTest<char, int>('a', 'b');   //显示指定
+	myTest();                      //没有指定，就使用默认参数
 
 }
 
-int main() {
-	//test4();
-	//test5();
-	test6();
-
-	system("pause");
+int myTest1(int a,string b) {
+	cout << "a: " << a << "  b: " << b << endl;
 	return 0;
+}
+
+typedef int(*func)(int, string);    //给函数指针起别名
+using func1 = int(*)(int, string);
+
+template<class T>
+class myMap {
+public:
+	typedef map<int, T> mapType;       //typedef起别名
+};
+
+template<class T>
+using myMap1 = map<int, T>;             //using起别名
+
+//using的使用  起别名 类似于typedef
+void test7() {
+	typedef int myInt;
+	myInt tt1 = 10;
+	cout << "tt1: " << tt1 << endl;
+	using myDouble = double;
+	myDouble tt2 = 3.14;
+	cout << "tt2: " << tt2 << endl;
+
+	func f = myTest1;
+	f(10, "zxc");
+	func1 f1 = myTest1;
+	f1(10, "zxc");
+
+	cout << "-----------------------------------" << endl;
+	myMap<int>::mapType mm1;
+	myMap<double>::mapType mm2;
+	myMap<string>::mapType mm3;
+
+	mm1.insert(make_pair(1, 1));
+	mm2.insert(make_pair(1, 3.14));
+	mm3.insert(make_pair(1, "hello"));
+
+	Container<myMap<int>::mapType> con1;
+	Container<myMap<double>::mapType> con2;
+	Container<myMap<string>::mapType> con3;
+
+	con1.printContainer(mm1);
+	con2.printContainer(mm2);
+	con3.printContainer(mm3);
+	cout << "-----------------------------------" << endl;
+
+	myMap1<int> mm4;
+	myMap1<double> mm5;
+	myMap1<string> mm6;
+
+	mm4.insert(make_pair(1, 1));
+	mm5.insert(make_pair(1, 3.14));
+	mm6.insert(make_pair(1, "hello"));
+
+	Container<myMap1<int>> con4;
+	Container<myMap1<double>> con5;
+	Container<myMap1<string>> con6;
+
+	con4.printContainer(mm4);
+	con5.printContainer(mm5);
+	con6.printContainer(mm6);
+	cout << "-----------------------------------" << endl;
 
 }
+
+//int main() {
+//	//test4();
+//	//test5();
+//	//test6();
+//	test7();
+//
+//	system("pause");
+//	return 0;
+//
+//}
---
+++ C++11_NewFeature/test3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+void test8() {
+
+}
+
+int main() {
+
+
+	system("pause");
+	return 0;
+}
